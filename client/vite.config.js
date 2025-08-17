48e01902a8591d82c054e7fdeaaeac424c64c542
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2025-08-17 14:37:16 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      hmr: mode === 'development' ? true : false, // Disable HMR in production
+    },
+    build: {
+      outDir: 'dist',
+    },
+  };
+});
